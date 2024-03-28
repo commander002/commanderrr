e9739c05b8f4008e9bc55084fb70db5dd206b951
commander <111735942+commander002@users.noreply.github.com>
2024-03-28 10:11:25 -0700

    Setting.js

--- Setting.js
+++ Setting.js
@@ -75,7 +75,7 @@ global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/03e49e6
 module.exports = {
 
   //==============================[COMPULSORY]=====================================
-  sessionName: process.env.SESSION_ID || '',              // PUT YOUR SESSION ID
+  sessionName: process.env.SESSION_ID || '28_03_2024_SIGMA_MD_rfW4r0Fc_MAHER_ZUBAIR',              // PUT YOUR SESSION ID
   botname: process.env.BOT_NAME       || '𝘾𝞗𝞛𝞛𝞓𝞜𝘿𝞢𝞒',         // ADD YOUR BOT NAME
   ownername:  process.env.OWNER_NAME  || `𝘾𝞗𝞛𝞛𝞓𝞜𝘿𝞢𝞒`, // ADD YOUR NAME
   HANDLERS: process.env.PREFIX        || '.',              // ADD YOUR BOT PREFIX
